739b16eeb8da5d8d608b3ac3f81a4c0b3abb1665
Thorsten Beitz <thorstenbeitz@t-online.de>
2020-06-22 14:19:28 +0200

    Changed to read all 35 elements of file header and saving values to a list

--- BinaryReader.py
+++ BinaryReader.py
@@ -2,10 +2,18 @@ import struct
 
 location = "Desktop/BinaryReader/Files/d20190923/20190923-T1.wdq"
 bin_data = open(location, "rb")
-formats = ["H", "H", "b", "b", "h", "I", "I", "h"]
+formats1 = ["H", "H", "b", "b", "h", "L", "L", "h", "H", "H", "l", "4b", "d",
+ "l", "l", "l", "l", "l", "l", "h", "h", "b", "b", "b", "b", "32b", "H", "H", "b", "b", "i", "b", "b"]
 values = []
 
-for form in formats:
+for form in formats1:
     values.append(struct.unpack(form, bin_data.read(struct.calcsize(form))))
 
-print(values)
+for i in range(110, values[4][0] - 3):
+    values.append(struct.unpack("36b", bin_data.read(struct.calcsize("36b"))))
+    i = i + 36
+
+values.append(struct.unpack("h", bin_data.read(struct.calcsize("h"))))
+
+
+print(values, len(values))
