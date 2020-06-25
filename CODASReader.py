51c551f7df6f1748f7c806eb76e577bec4391862
Thorsten Beitz <thorstenbeitz@t-online.de>
2020-06-25 20:57:30 +0200

    Changed code structure Removed TranslatedFile class Made CODASReader an object that stores translated parts of file Moved all methods from TranslatedFile to CODASReader class Separated reading of header, adc data, and trailer into 3 separate methods

--- CODASReader.py
+++ CODASReader.py
@@ -4,36 +4,31 @@ import numpy as np
 
 
 class CODASReader:
-    """Reads CODAS files and converts them into ASCII. \n
-    Information on the file format can be found at 
-    https://www.dataq.com/resources/techinfo/ff.htm."""
+    """Object to read, translate, store and write content from
+    CODAS files. \n
+    Information on the file format can be found at \n
+    https://www.dataq.com/resources/techinfo/ff.htm"""
 
-    field_names = ["S/R denom", "Intelligent Oversampling Factor", "Byte 4",
-                   "Byte 5", "Bytes in data file header", "Byte 8 - 11",
-                   "Byte 12 - 15", "User annotation Bytes",
-                   "Height of graphics area", "Width of graphics area",
-                   "Cursor position (screen)", "Byte 24 - 27",
-                   "Time between samples", "Time file openened",
-                   "Time trailer written", "Waveform compression factor",
-                   "Byte 48 - 51", "Cursor position (file)",
-                   "time marker position (file)", "Left limit cursor",
-                   "Right limit cursor", "Byte 64", "Byte 65", "Byte 66",
-                   "Byte 67", "Byte 68 - 99", "Byte 100 - 101",
-                   "Byte 102 - 103", "Byte 104", "Byte 105",
-                   "Byte 106 - 107", "Byte 108", "Byte 109"]
+    bytes_in_file = 0
+    header = []
+    adc_data = []
+    trailer = []
+    packed = False
+    hiRes = False
+    acq_channels = 0
+    adc_data_bytes = 0
 
-    # takes one string argument for file location,
-    # returns TranslatedFile object
-    @staticmethod
-    def readFromFile(location):
-        """param location : str \n
-        returns TranslatedFile type \n
-        Reads CODAS file from file location 'location' 
-        and converts it to ASCII."""
-
+    def __init__(self, location, read_header=True):
         bin_data = open(location, "rb")
         bin_data.seek(0, 2)
-        bytes_in_file = bin_data.tell()
+        self.bytes_in_file = bin_data.tell()
+        bin_data.seek(0, 0)
+        bin_data.close()
+        if read_header:
+            self.readHeader()
+
+    def readHeader(self):
+        bin_data = open(location, "rb")
         bin_data.seek(0, 0)
 
         # format strings for first 33 elements of header,
@@ -49,7 +44,7 @@ class CODASReader:
                     False, False, False, False, False, False, False, False,
                     False, False, False, False, False, True, True, False,
                     True, False, True, True, True, True, True, True, True]
-        values = []
+        self.header = []
         # format strings for the elements in the channel information
         channel_format = ["<f", "<f", "<d", "<d",
                           "<6b", "<b", "<b", "<b", "<b", "<H"]
@@ -60,129 +55,143 @@ class CODASReader:
 
         # reads and converts the first 33 elements of the file header
         for i, form in enumerate(formats):
-            values.append(struct.unpack(
+            self.header.append(struct.unpack(
                 form, bin_data.read(struct.calcsize(form))))
             # reducing 1 element tuples to single element
-            if len(values[-1]) == 1:
-                values[-1] = values[-1][0]
+            if len(self.header[-1]) == 1:
+                self.header[-1] = self.header[-1][0]
             # converting all elements meant to be read bitwise to binary
             if bin_bool[i]:
-                values[i] = (
+                self.header[i] = (
                     "{0:0" + str(struct.calcsize(form) * 8)
-                    + "b}").format(values[i])
+                    + "b}").format(self.header[i])
 
         # reads and converts element 34 which contains
         # channel information for all channels
-        # values[4] stores total number of bytes in file header,
-        # values[3] stores number of bytes per channel,
-        # values[2] stores start of channel information
-        for i in range(int((values[4] - values[2] - 2)/values[3])):
-            channel_values = []
+        # header[4] stores total number of bytes in file header,
+        # header[3] stores number of bytes per channel,
+        # header[2] stores start of channel information
+        for i in range(int((self.header[4] - self.header[2] - 2)
+                           / self.header[3])):
+            channel_info = []
             # creates the substructure of element 34
             for k, form in enumerate(channel_format):
-                channel_values.append(struct.unpack(
+                channel_info.append(struct.unpack(
                     form, bin_data.read(struct.calcsize(form))))
                 # reducing 1 element tuples to single element
-                if len(channel_values[-1]) == 1:
-                    channel_values[-1] = channel_values[-1][0]
+                if len(channel_info[-1]) == 1:
+                    channel_info[-1] = channel_info[-1][0]
                 # converting all elements meant to be read
                 # bitwise to binary
                 if bin_bool_channel[k]:
-                    channel_values[k] = (
+                    channel_info[k] = (
                         "{0:0" + str(struct.calcsize(form) * 8)
-                        + "b}").format(channel_values[k])
-            values.append(channel_values)
+                        + "b}").format(channel_info[k])
+            self.header.append(channel_info)
 
         # reading and converting last header element
-        values.append(struct.unpack(
+        self.header.append(struct.unpack(
             "<h", bin_data.read(struct.calcsize("<h")))[0])
-
-        # checking if the file is packed (stored in values[26][1])
-        packed = False
-        if int(values[26][1]) == 1:
-            packed = True
+        # checking if the file is packed (stored in self.header[26][1])
+        if int(self.header[26][1]) == 1:
+            self.packed = True
 
         # number of acquired channels
-        # (stored in the last 5 (or 8) bits of values[0])
-        if int(values[0][7]) == 0:
-            acq_channels = int(values[0][-5:], 2)
+        # (stored in the last 5 (or 8) bits of self.header[0])
+        if int(self.header[0][7]) == 0:
+            self.acq_channels = int(self.header[0][-5:], 2)
         else:
-            acq_channels = int(values[0][-8:], 2)
+            self.acq_channels = int(self.header[0][-8:], 2)
 
         # determining length of adc data in file
         # which is dependent on whether file is packed or not
-        # values[5] stores total number of bytes for adc storage
-        # values[33] corresponds to first channel information,
-        # values[33][6] gives sample rate divider
-        if packed:
+        # self.header[5] stores total number of bytes for adc storage
+        # self.header[33] corresponds to first channel information,
+        # self.header[33][6] gives sample rate divider
+        if self.packed:
             sample_sum = 0
             # calculate samples per channel
             # and then sum them up over all channels
-            for i in range(acq_channels):
+            for i in range(self.acq_channels):
                 sample_sum = int(
-                    sample_sum + ((values[5] / acq_channels) - 1)
-                    / values[33 + i][6]) + 1
-            adc_data_lim = int(2 * sample_sum)
+                    sample_sum + ((self.header[5] / self.acq_channels) - 1)
+                    / self.header[33 + i][6]) + 1
+            self.adc_data_bytes = int(2 * sample_sum)
         else:
-            adc_data_lim = int(values[5])
+            self.adc_data_bytes = int(self.header[5])
 
         # determining whether file is hiRes and uses 16 bit data
-        # (stored in bit 14 of values[26])
-        if int(values[26][14]) == 1:
-            hiRes = True
+        # (stored in bit 14 of self.header[26])
+        if int(self.header[26][14]) == 1:
+            self.hiRes = True
+
+        bin_data.close
+
+    def readADC(self):
+        bin_data = open(location, "rb")
+        bin_data.seek(self.header[4], 0)
+        if self.hiRes:
             adc_bit_length = 16
-            channels_hiRes = 1
         else:
-            hiRes = False
             adc_bit_length = 14
-            channels_hiRes = acq_channels
 
         # creating the adc data from the main body of the binary file
-        adc_data = []
-        for i in range(int(adc_data_lim / (2 * acq_channels))):
+        self.adc_data = []
+        for i in range(int(self.adc_data_bytes / (2 * self.acq_channels))):
             # reading 2 bytes per data point per channel
             # as unsigned short, then converting it into binary string
             adc_data_item = []
-            for k in range(acq_channels):
+            for k in range(self.acq_channels):
                 adc_data_bin = "{0:016b}".format(
                     struct.unpack("<H", bin_data.read(2))[0])
                 # determining the sign of the binary number
                 # by looking for bit 0,
                 # then corectly converting the relevant bits back
                 # to decimal and adding them to item list
-                # values[33] corresponds to first channel information,
-                # values[33][2] calibration scaling factor
+                # self.header[33] corresponds to first channel info,
+                # self.header[33][2] calibration scaling factor
                 if int(adc_data_bin[0]) == 0:
                     adc_data_bin = (int(adc_data_bin[1:adc_bit_length], 2)
-                                    * values[33 + k][2])
+                                    * self.header[33 + k][2])
                 else:
                     # converting negative two's complement binary
                     # to decimal
                     adc_data_bin = ((int(adc_data_bin[1:adc_bit_length], 2)
                                      - (1 << len(adc_data_bin[1:adc_bit_length]
-                                                 ))) * values[33 + k][2])
+                                                 ))) * self.header[33 + k][2])
                 adc_data_item.append(adc_data_bin)
             # appending date and time to the item list,
-            # values[13] stores time of start of measurement,
-            # values[12] stores time between measurements
+            # self.header[13] stores time of start of measurement,
+            # self.header[12] stores time between measurements
             adc_data_item.append(time.strftime(
-                "%d %b %Y", time.gmtime(values[13] + i * values[12])))
+                "%d %b %Y", time.gmtime(self.header[13] + i * self.header[12])))
             adc_data_item.append(time.strftime(
-                "%H:%M:%S", time.gmtime(values[13] + i * values[12])))
+                "%H:%M:%S", time.gmtime(self.header[13] + i * self.header[12])))
             # appending all values to adc_data list which will be stored
-            adc_data.append(adc_data_item)
+            self.adc_data.append(adc_data_item)
 
+        bin_data.close()
+
+    def readTrailer(self):
+
+        bin_data = open(location, "rb")
+        bin_data.seek(self.header[4] + self.adc_data_bytes, 0)
         # translating the trailer of the file
-        trailer = []
+        self.trailer = []
         trailer_pointers = []
         trailer_item = []
         marker = True
         time_stamp = False
+
+        if self.hiRes:
+            channels_hiRes = 1
+        else:
+            channels_hiRes = self.acq_channels
         # translating first part of trailer containing
         # event marker pointers
-        # values[6] stores total number of bytes in trailer part 1,
+        # self.header[6] stores total number of bytes in trailer part 1,
         # each entry is a 4 byte long
-        for i in range(int(values[6] / 4)):
+        for i in range(int(self.header[6] / 4)):
             if marker:
                 trailer_item = [struct.unpack("<l", bin_data.read(4))[0]]
                 marker = False
@@ -197,7 +206,7 @@ class CODASReader:
                 # determine whether next long is comment pointer
                 # or new marker pointer
                 trailer_long = struct.unpack("<l", bin_data.read(4))[0]
-                if trailer_long > -1 * values[5] / (2 * channels_hiRes):
+                if trailer_long > -1 * self.header[5] / (2 * channels_hiRes):
                     trailer_pointers.append(trailer_item)
                     trailer_item = [trailer_long]
                     if trailer_item[-1] >= 0:
@@ -212,15 +221,15 @@ class CODASReader:
             trailer_pointers.append(trailer_item)
         elif trailer_pointers[-1] != trailer_item:
             trailer_pointers.append(trailer_item)
-        trailer.append(trailer_pointers)
+        self.trailer.append(trailer_pointers)
 
         # translating second part of trailer
         # containing user annotations,
         # null character (0) ends each annotation
-        # values[7] stores number of user annotations
+        # self.header[7] stores number of user annotations
         trailer_annotations = []
         trailer_item = ""
-        for i in range(values[7]):
+        for i in range(self.header[7]):
 
             trailer_byte = struct.unpack("<b", bin_data.read(1))[0]
             if int(trailer_byte) == 0:
@@ -228,53 +237,51 @@ class CODASReader:
                 trailer_item = ""
             else:
                 trailer_item = trailer_item + chr(int(trailer_byte))
-        trailer.append(trailer_annotations)
+        self.trailer.append(trailer_annotations)
 
         # translating all remaining bytes as
         # event marker comment part of the trailer,
         # code works similiarly to user annotation above
         trailer_item = ""
         trailer_comments_list = []
-        #trailer_comments = bin_data.read()
-        for i in range(bytes_in_file - (values[4] + adc_data_lim
-                                        + values[6] + values[7])):
+        # trailer_comments = bin_data.read()
+        for i in range(self.bytes_in_file - (self.header[4]
+                                             + self.adc_data_bytes
+                                             + self.header[6]
+                                             + self.header[7])):
             trailer_byte = struct.unpack("<b", bin_data.read(1))[0]
             if int(trailer_byte) == 0:
                 trailer_comments_list.append(trailer_item)
                 trailer_item = ""
             else:
                 trailer_item = trailer_item + chr(int(trailer_byte))
-        trailer.append(trailer_comments_list)
+        self.trailer.append(trailer_comments_list)
 
         bin_data.close()
-
-        # returns a TranslatedFile object with the translated
-        # three main parts of the file as attributes
-        return(TranslatedFile(values, adc_data, trailer))
-
-
-class TranslatedFile:
-    """Object containing separate lists for the header,
-     ADC data and trailer of file in ASCII."""
-    header = []
-    adc_data = []
-    trailer = []
-
-    # creating a new translated file with lists for the header,
-    # adc_data and trailer of the file
-    def __init__(self, header, adc_data, trailer):
-        self.header = header
-        self.adc_data = adc_data
-        self.trailer = trailer
 
     # printing list with header values
     def printHeader(self):
         """Prints header of the file"""
+
+        field_names = ["S/R denom", "Intelligent Oversampling Factor",
+                       "Byte 4", "Byte 5", "Bytes in data file header",
+                       "Byte 8 - 11", "Byte 12 - 15", "User annotation Bytes",
+                       "Height of graphics area", "Width of graphics area",
+                       "Cursor position (screen)", "Byte 24 - 27",
+                       "Time between samples", "Time file openened",
+                       "Time trailer written", "Waveform compression factor",
+                       "Byte 48 - 51", "Cursor position (file)",
+                       "time marker position (file)", "Left limit cursor",
+                       "Right limit cursor", "Byte 64", "Byte 65", "Byte 66",
+                       "Byte 67", "Byte 68 - 99", "Byte 100 - 101",
+                       "Byte 102 - 103", "Byte 104", "Byte 105",
+                       "Byte 106 - 107", "Byte 108", "Byte 109"]
+
         for i in range(len(self.header) - 1):
-            if i < len(CODASReader.field_names):
-                print(CODASReader.field_names[i] + ": " + str(self.header[i]))
+            if i < len(field_names):
+                print(field_names[i] + ": " + str(self.header[i]))
             else:
-                print("Channel No. " + str(i - len(CODASReader.field_names))
+                print("Channel No. " + str(i - len(field_names))
                       + " information: "
                       + str(self.header[i]))
         print("Fixed value of 8001H: " + str(self.header[-1]))
@@ -285,7 +292,7 @@ class TranslatedFile:
         param delim : str, optional \n
         param fmt : str, optional \n
         Saves the ADC data of the file to a CSV file of name 'name'."""
-        #np.savetxt(name, self.adc_data, delimiter = delim, fmt=fmt)
+        # np.savetxt(name, self.adc_data, delimiter = delim, fmt=fmt)
         with open(name, "w", newline="\n") as file:
             for item in self.adc_data:
                 for seg in item:
@@ -294,7 +301,6 @@ class TranslatedFile:
                 file.write("\n")
 
     # printing the trailer element of the file
-
     def printTrailer(self):
         """Prints the trailer of the file"""
         print("Event marker pointers: ")
@@ -328,10 +334,7 @@ class TranslatedFile:
     # (stored in last 5 (or 8) bits in header[0])
     def printAcqChannels(self):
         """Prints number of acquired channels"""
-        if int(self.header[0][7]) == 0:
-            print(int(self.header[0][-5:], 2))
-        else:
-            print(int(self.header[0][-8:], 2))
+        print(self.acq_channels)
 
     # print channel information for either one channel
     # or a list of channel numbers, default is all channels
@@ -352,9 +355,11 @@ class TranslatedFile:
 
 
 location = "Desktop/BinaryReader/Files/20190923-T1.wdq"
-trans_file = CODASReader.readFromFile(location)
-trans_file.printHeader()
-trans_file.saveADCsToCSV("Desktop/BinaryReader/Files/output1.csv")
-trans_file.printTrailer()
-trans_file.printChannelInfo([1, 2])
-trans_file.printAcqTime()
+
+codas_reader = CODASReader(location)
+codas_reader.readADC()
+codas_reader.readTrailer()
+codas_reader.saveADCsToCSV("Desktop/BinaryReader/Files/output1.csv")
+codas_reader.printHeader()
+codas_reader.printTrailer()
+codas_reader.printAcqChannels()
