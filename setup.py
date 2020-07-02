b251bdc7969fa8cb73bd35407feb194177731c21
Thorsten Beitz <thorstenbeitz@t-online.de>
2020-07-02 16:55:24 +0200

    Removed packages property

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author_email='thorsten.beitz@ucdconnect.ie',
     maintainer='John Quinn',
     maintainer_email='john.quinn@ucd.ie',
-    packages=['CODASReader'],
     license='LICENSE.txt',
     description='Python class to read CODAS files and translate to ASCII',
     long_description=open('README.txt').read(),
